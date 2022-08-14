36de4ae88463e412e0b75cb9eba97f16270ed0c0
Sonja Jamina <87620872+jaminas9@users.noreply.github.com>
2022-08-14 17:21:47 +0200

    Add files via upload

---
+++ RightHandFingerCounterMain.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 14 11:06:18 2022
+
+@author: Sonja
+"""
+
+import cv2
+import time
+import os
+import handTrackingModule as htm
+
+
+cap = cv2.VideoCapture(0)
+wCam, hCam = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+folderPath = "C:/Users/Sonja/Desktop/fingers"
+myList = os.listdir(folderPath)
+print(myList)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+   
+    overlayList.append(image)
+
+print(len(overlayList))
+pTime = 0
+
+detector = htm.handDetector(detectionCon=0.75)
+
+tipIds = [4, 8, 12, 16, 20] #top points for each finger
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        
+        # Thumb for right hand only
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]: # is it on right or left side of spot bellow 
+        
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 Fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]: #finger open
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        #print(fingers)
+        totalFingers = fingers.count(1) #find how many 1 we have in list
+        #print(totalFingers)
+        #getting images from number if it 5 we get image with id 4 --<<2
+        h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers]
+        
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                    10, (255, 0, 0), 25)
+        
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+
+    cv2.imshow("FingerCounter", img)
+    cv2.waitKey(1) #1 ms delay
